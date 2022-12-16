c1235ef26a6acb35bcb1521aed7c417034d9465c
Joshua Estes <jne0010@uah.edu>
2022-12-16 00:46:11 -0600

    Refactored code to make functions for modular

--- src/commands/delete.rs
+++ src/commands/delete.rs
@@ -1,13 +1,11 @@
+use super::pq;
 use serenity::builder::CreateApplicationCommand;
+use serenity::model::prelude::command::CommandOptionType;
 use serenity::model::prelude::interaction::application_command::ApplicationCommandInteraction;
 use serenity::model::prelude::interaction::application_command::CommandDataOptionValue;
-use serenity::model::prelude::command::CommandOptionType;
-use super::super::schema::team_seaking::dsl::*;
-use diesel::prelude::*;
-use super::pq;
 
 pub fn delete_record(command_interaction: &mut ApplicationCommandInteraction) -> String {
-    let connection = &mut pq::connect::establish_connection();
+    let conn = &mut pq::connect::establish_connection();
 
     let options = &command_interaction.data.options;
 
@@ -26,36 +24,38 @@ pub fn delete_record(command_interaction: &mut ApplicationCommandInteraction) ->
         return format!("{:?} is not a valid id", name_option);
     }
 
-	let num_deleted = diesel::delete(team_seaking.filter(discordname.like(search)))
-        .execute(connection)
-        .expect("Error deleting posts");
+    let num_deleted = pq::interface::delete_row(conn, search);
 
     format!("Deleted {} post", num_deleted)
 }
 
 pub fn delete_table(_command_interaction: &mut ApplicationCommandInteraction) -> String {
-    let connection = &mut pq::connect::establish_connection();
+    let conn = &mut pq::connect::establish_connection();
 
-    let result = diesel::delete(team_seaking).execute(connection);
+    let result = pq::interface::delete_table(conn);
 
     format!("Delete returned \"{:?}\"", result)
 }
 
-pub fn register_table_delete(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
+pub fn register_table_delete(
+    command: &mut CreateApplicationCommand,
+) -> &mut CreateApplicationCommand {
     command
         .name("delete_table")
         .description("Deletes all rows in a table")
 }
 
-pub fn register_row_delete(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
+pub fn register_row_delete(
+    command: &mut CreateApplicationCommand,
+) -> &mut CreateApplicationCommand {
     command
         .name("delete_row")
         .description("Delete a row with the specified id")
         .create_option(|option| {
-			option
-				.name("username")
-				.description("Discord user's username")
-				.kind(CommandOptionType::String)
-				.required(true)
-		})
+            option
+                .name("username")
+                .description("Discord user's username")
+                .kind(CommandOptionType::String)
+                .required(true)
+        })
 }
--- src/commands/pq/interface.rs
+++ src/commands/pq/interface.rs
@@ -1,19 +1,30 @@
+use super::super::super::schema::team_seaking::dsl::*;
 use diesel::prelude::*;
 
 use super::super::super::models::TeamSeaking;
 
-pub fn insert(conn: &mut PgConnection, id: &i64, name: &str, discord_name: &str, description: &str) -> TeamSeaking {
+pub fn insert(conn: &mut PgConnection, id_loc: &i64, name_loc: &str, discord_name: &str, description_loc: &str) -> TeamSeaking {
     use crate::schema::team_seaking;
 
     let new_seaker = TeamSeaking {
-        id: * id,
-		name: String::from(name),
+        id: * id_loc,
+		name: String::from(name_loc),
 		discordname: String::from(discord_name),
-		description: String::from(description),
+		description: String::from(description_loc),
     };
 
     diesel::insert_into(team_seaking::table)
         .values(&new_seaker)
         .get_result(conn)
         .expect("Error saving new seaker")
+}
+
+pub fn delete_row(conn: &mut PgConnection, search: String) -> usize {
+    diesel::delete(team_seaking.filter(discordname.like(search)))
+        .execute(conn)
+        .expect("Error deleting posts")
+}
+
+pub fn delete_table(conn: &mut PgConnection) -> Result<usize, diesel::result::Error> {
+    diesel::delete(team_seaking).execute(conn)
 }
