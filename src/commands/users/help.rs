8e8921dfbc9b94ab87989ffec605fdf973882157
Joshua Estes <jne0010@uah.edu>
2022-12-20 23:40:35 -0600

    Changed help msg

--- src/commands/users/help.rs
+++ src/commands/users/help.rs
@@ -1,18 +1,17 @@
 use serenity::builder::CreateApplicationCommand;
 
 pub fn help() -> String {
-    let help_message = "I can help you explore and join groups.
-    **The following is a list of available commands.**
-    `!help`: Show this message.
-    `!whoami`: See some info about you.
-    `!checkin <description>`: Register as \"Team seeking\". Provide a description of yourself so others can read up on you!
-    `!ls`: List all groups.
-    `!ls_free`: List all available registered users.
-    `!create <group_name> <description>`: Create a group and join it. Your `group_name` may not contain spaces.
-    `!join <group_name>`: Join an existing group.
-    `!describe <group_name>`: Show group summary.";
+	let part1 = "**The following is a list of available commands.**";
+    let part2 = "`!help`: Show this message.";
+    let part3 = "`!whoami`: See some info about you.";
+    let part4 = "`!checkin <description>`: Register as \"Team seeking\". Provide a description of yourself so others can read up on you!";
+    let part5 = "`!ls`: List all groups.";
+    let part6 = "`!ls_free`: List all available registered users.";
+    let part7 = "`!create <group_name> <description>`: Create a group and join it. Your `group_name` may not contain spaces.";
+    let part8 = "`!join <group_name>`: Join an existing group.";
+    let part9 = "`!describe <group_name>`: Show group summary.";
 
-    String::from(help_message)
+    format!("{}\n\t{}\n\t{}\n\t{}\n\t{}\n\t{}\n\t{}\n\t{}\n\t{}\n", part1, part2, part3, part4, part5, part6, part7, part8, part9)
 }
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
